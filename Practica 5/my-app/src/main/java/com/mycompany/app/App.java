73eab0280cdd05e90f9020c747a857b11530ad30
davpro1 <abndtz22@users.noreply.github.com>
2020-10-21 08:24:45 -0500

    Agregada Practica 6 completa

--- Practica 5/my-app/src/main/java/com/mycompany/app/App.java
+++ Practica 5/my-app/src/main/java/com/mycompany/app/App.java
@@ -10,6 +10,23 @@ public class App
     public static void main( String[] args )
     {
         port(8081);
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
         get("/",(request,response)-> "Hola desde Spark");
         get("/adios",(request,response)-> "Adios desde Spark");
     }
